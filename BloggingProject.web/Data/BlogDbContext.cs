6bad60e21707115d651fe8ae6e075da2f9de5e43
Audacity01 <vaibhavjoshi@fincart.com>
2024-03-21 00:33:18 +0530

    project creation, initialize DbContext

---
+++ BloggingProject.web/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BloggingProject.web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggingProject.web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ BloggingProject.web/Models/Domain/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace BloggingProject.web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Displayname { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
