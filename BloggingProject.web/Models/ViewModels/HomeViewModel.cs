b3d67a156cce72781ea3388115f666da6eec7ab2
Audacity01 <vaibhavjoshi@fincart.com>
2024-03-23 00:06:41 +0530

    views completed for home page, login and register(some bug)

---
+++ BloggingProject.web/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BloggingProject.web.Models.Domain;
+
+namespace BloggingProject.web.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<BlogPost> BlogPosts { get; set; }
+
+        public IEnumerable<Tag> Tags { get; set; }
+    }
+}
---
+++ BloggingProject.web/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BloggingProject.web.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public List<User> Users { get; set; }
+        public string Username { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public bool AdminRoleCheckbox { get; set; }
+    }
+}
